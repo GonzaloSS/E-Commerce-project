a31086a4051dba6d4b9ce25e772261081006cc18
GonzaloSS <gonzalosantanasantana3@gmail.com>
2020-11-24 15:27:19 +0000

    backend updated

--- E-CommerceIonicNode/backend/app/models/index.js
+++ E-CommerceIonicNode/backend/app/models/index.js
@@ -19,7 +19,30 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
+////////////////////////////////////////
+
+
+
+db.user = require("./user.model.js")(sequelize, Sequelize);
 db.address = require("./address.model.js")(sequelize, Sequelize);
 db.products = require("./products.model.js")(sequelize, Sequelize);
+db.order = require("./order.model.js")(sequelize, Sequelize);
+db.orderProduct = require("./orderProduct.model.js")(sequelize, Sequelize);
+
+
+///Associations///
+
+//Foreign Key for user's table//
+db.address.hasMany(db.user, {as: 'users', foreignKey: 'id_address'});
+////////////////////////////////
+
+//Foreign Key for order's table//
+db.user.hasMany(db.order, {as: 'orders', foreignKey: 'id_user'});
+////////////////////////////////
+
+//Foreign Key for orderProducts's table//
+db.products.hasMany(db.orderProduct, {as: 'orderproducts', foreignKey: 'id_product'});
+db.order.hasMany(db.orderProduct, {as: 'orderproducts', foreignKey: 'id_order'});
+////////////////////////////////
 
 module.exports = db;
